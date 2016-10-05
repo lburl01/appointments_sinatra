7813814136e47641df9b87c18b28f9e620bcf949
Lori Burleson <lori.burleson@gmail.com>
2016-10-05 18:46:57 -0400

    Lots of changes. No idea where I am...

--- api_appointments.rb
+++ api_appointments.rb
@@ -1,8 +1,10 @@
 require 'sinatra'
 require_relative 'model'
 require 'json'
+require_relative 'schema'
+require 'pry'
 
-get '/api/appointments' do
+get '/api/all/appointments' do
   Appointment.all.to_json
 end
 
@@ -24,23 +26,43 @@ get '/api/patients/:id' do
   status 200
 end
 
+get '/api/appointments/get/:patient_id' do
+  appointment = Appointment.includes(params['patient_id'])
+  if appointment.nil?
+    halt(404)
+  end
+  appointment.to_json
+  status 200
+end
+
+get '/api/appointments/:physician_id' do
+  Physician.find(Appointment.where(physician_id: params['physician_id']).first.physician_id).to_json
+end
+
+get '/api/appointments/all/:physician_id' do
+  #appts = Appointment.where(physician_id: params['physician_id']).to_json
+  Physician.find_each do |physician|
+    Appointment.where(physician_id: params['physician_id']).to_json
+  end
+end
+
 post '/api/appointments' do
   Appointment.create(physician_id: params['physician_id'], patient_id: params['patient_id'], appointment_date: Time.now).to_json
   status 201
 end
-#
-# put '/api/tasks/:id' do
-#   t = Task.find_by(id: params[:id])
-#   if t.nil?
-#     halt(404)
-#   end
-# 	t.update(
-# 		description: 	params['description'],
-#     priority: params['priority'],
-#     completed: params['completed']
-# 		).to_json
-# end
-#
+
+post '/api/physicians' do
+  Physician.create(dr_name: params['dr_name']).to_json
+  status 201
+end
+
+put '/api/physicians/update/:id' do
+  p = Physician.find_by(id: params[:id])
+  p.update(
+		dr_name: 	'Dr. Lori'
+		).to_json
+end
+
 # delete '/api/tasks/:id' do
 #   t = Task.find_by(id: params[:id])
 #   if t.nil?
